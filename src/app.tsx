1c09154ee01d71b437540c6bd068c68a02e103e1
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-04 16:36:42 +1000

    af

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink, useLocation  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, useLocation, Navigate  } from "react-router-dom";
 import { MenuBar } from "./menu_bar.js";
 import { MenuNavLinkDisplay } from "./menu_bar.js";
 import { ArpanetPage } from "./arpanet.js";
-import { useContext } from "react";
-export interface myAppContext {
-    
+import { NotFound } from "./notfound.js";
+import { useContext, useState } from "react";
+export interface MyAppContext {
+    navigation: [string, React.Dispatch<React.SetStateAction<string>>]
 }
-const AppContext = React.createContext({});
+const AppContext = React.createContext<MyAppContext | undefined>(undefined);
 export function App() {
-    return <BrowserRouter>
+    const [navString, setNavString] = useState("/");
+    
+    return <AppContext.Provider value={{
+            navigation: [
+                navString, setNavString
+            ]
+        }}>            
+            <BrowserRouter>
                 <MenuBar/>
                 <MenuNavLinkDisplay/>
                 <Routes>
-                    <Route path="/" element={<ArpanetPage/>} />
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<ArpanetPage/>} />
                     <Route path="/about" element={<></>} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
+        </AppContext.Provider>
 }
--- src/menu_bar.tsx
+++ src/menu_bar.tsx
@@ -5,14 +5,15 @@ export interface MenuButtonProps extends React.ComponentProps<typeof NavLink> {
     children?: React.ReactNode
 }
 export function MenuButton({buttonProps, children, ...props}: MenuButtonProps) {
-    return <NavLink {...props}>
-        <button className="menuButton" {...buttonProps}>
-            {children}
-        </button>
+    return <NavLink className="menuButton noLink" {...props}>
+        {children}
     </NavLink>
 }
 export function MenuBar() {
-    return <header><p className="heading_logo standard">Connecting Me and You</p>
+    return <header>
+    <NavLink to="/home" className="heading_logo standard noLink">
+        Connecting Me and You
+    </NavLink>
         <div className="navbar">
             <MenuButton buttonProps={{}} to="/arpanet">Hello</MenuButton>
         </div></header>
