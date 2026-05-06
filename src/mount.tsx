4a74ed53ae2f1eb8fa041b751de3cb621d025381
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-07 09:36:31 +1000

    t

--- src/mount.tsx
+++ src/mount.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.js";
+
 const header = document.querySelector("body");
 if (!header) throw new Error("header does not exist");
 const root = createRoot(header);
@@ -8,4 +9,4 @@ root.render(
     <StrictMode>
         <App/>
     </StrictMode>
-);
+)
