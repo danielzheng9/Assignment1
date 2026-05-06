e5193c3b8a89e2fc4a67442ff01a8c83de0f0ab6
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-07 09:50:13 +1000

    b

--- src/pages/bandwidth.tsx
+++ src/pages/bandwidth.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import lowVsHighBandwidth from '../media/lowvshighbandwidth.png'; 
+
 export function WhatIsNetworkBandwidth() {
     return <main>
         <h1>What is network bandwidth?</h1>
         <p>Network bandwidth is a measurement indicating the maximum capacity of a wired or wireless communications link to transmit data over a network connection in a given amount of time.</p>
         <h2>Think of it like this.</h2>
         <p>Your bandwidth is the width of the funnel. The data is liquid going in from the funnel. The larger the bandwidth, the more liquid can get in to the exit. (Liquid is not friendly with technology, still)</p>
-        <img className="behaveImage" src="/lowvshighbandwidth.png"></img>
+        <img className="behaveImage" src={lowVsHighBandwidth}></img>
     </main>;
 }
