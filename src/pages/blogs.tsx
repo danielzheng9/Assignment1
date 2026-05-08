0d33e46fe07d0366327c88c257dc8498b0fafd35
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-08 21:27:14 +1000

    Update blogs.tsx

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -3,7 +3,7 @@ import { Blogs, type BlogType } from "../blogsComponent.js";
 export function BlogsPage({blogs}: {blogs: BlogType}) {
     return <>
     <h1>Definitions</h1>
-    <p>Click on a definition to go to it.</p>
+    <p>Click on a definition to read and understand.</p>
     <Blogs blogs={blogs}/>
     </>
 }
