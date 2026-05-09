83f6bd6a479be3e4673d6d7b4ddd3ba1d57b2992
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-09 12:05:07 +1000

    Added search bar for definitions

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { Blogs, type BlogType } from "../blogsComponent.js";
+import React, { useState } from "react";
+import { Blogs, type Blog, type BlogType } from "../blogsComponent.js";
 export function BlogsPage({blogs}: {blogs: BlogType}) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredBlogs: BlogType = [...blogs].filter((thisBlog: Blog, index: number) => {
+        for (const [key, value] of Object.entries(thisBlog)) {
+            const stringifiedValue: string = value.toString();
+            if (!stringifiedValue.toLowerCase().includes(searchTerm.toLowerCase())) continue;
+            return true;
+        }
+        return false;
+    })
     return <>
-    <h1>Definitions</h1>
+    <h1 className="searchBarAndTitle">Definitions <input className="search" type="text" placeholder="Search" onChange={(event: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        const target = event.target;
+        setSearchTerm(target.value);
+    }}/></h1>
     <p>Click on a definition to read and understand.</p>
-    <Blogs blogs={blogs}/>
+    <Blogs blogs={filteredBlogs}/>
     </>
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 export function Home() {
     const beginningsOfInternet = useRef<HTMLHeadingElement | null>(null);
     return <div className="homeStripe">
+            <h1></h1>
             <h1 className="title">Me and You on the Web</h1>
             <p>Let's explore how the world wide web works!</p>
             <NavLink to="/definitions">Go to our definitions</NavLink>
@@ -12,7 +13,8 @@ export function Home() {
             
             <div className="FullHeightSpacer"/>
             <div className="flexNormal timelessPage">
-                <h1 ref={beginningsOfInternet}>The Beginnings of the Internet</h1>
+                <h1 ref={beginningsOfInternet}></h1>
+                <h1>The Beginnings of the Internet</h1>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Internet_map_1024_-_transparent%2C_inverted.png/330px-Internet_map_1024_-_transparent%2C_inverted.png"/>
                 <p className="ghost">an image of a portion of the internet connections [Source: <NavLink to="https://en.wikipedia.org/wiki/History_of_the_Internet">https://en.wikipedia.org/wiki/History_of_the_Internet</NavLink>]</p>
 
