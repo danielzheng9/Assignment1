e5f696820e44f8cb62a13d76a19d0102d311382e
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-10 13:22:48 +1000

    improved noscript

--- src/logo.tsx
+++ src/logo.tsx
@@ -3,14 +3,12 @@ import { useState, useEffect, useCallback, Component } from "react";
 import { tagAppend, type ClassyName } from "./utils/tagAppend.js";
 export function LogoFull({className,  ...props}: ClassyName & Partial<NavLinkProps>) {
     return <NavLink {...props} to="/" className={tagAppend("heading_logo standard noLink", className)}>
-        <img className="main-logo" 
-        src="https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/dognetTechnologies.png" alt="The full text version of our website logo"/>
+        <img className="main-logo" src="https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/dognetTechnologies.png" alt="The full text version of our website logo"/>
     </NavLink>
 }
 export function LogoIcon({className, ...props}: ClassyName & Partial<NavLinkProps>) {
     return <NavLink {...props} to="/" className={tagAppend("heading_logo standard noLink", className)}>
-        <img className="main-logo" 
-        src="https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/favicon.png" alt="The icon version of our website logo"/>
+        <img className="main-logo" src="https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/favicon.png" alt="The icon version of our website logo"/>
     </NavLink>
 }
 export function Logo() {
