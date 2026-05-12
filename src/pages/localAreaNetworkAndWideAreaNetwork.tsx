880464ccdfa05d573210f792a504a490e73191bd
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-12 18:19:43 +1000

    Update localAreaNetworkAndWideAreaNetwork.tsx

--- src/pages/localAreaNetworkAndWideAreaNetwork.tsx
+++ src/pages/localAreaNetworkAndWideAreaNetwork.tsx
@@ -19,6 +19,6 @@ export function LocalAreaNetworkAndWideAreaNetwork() {
             Wide Area Networks are used to connect computers across intercontinental locations. They are needed for large entities like businesses to transmit data across the globe—acting as a unified system to link an office to a central headquarter.
             Wide Area Networks are used to connect computers across intercontinental locations. They are needed for large entities like businesses to transmit data across the globe—acting as a unified system to link an office to another, or to a central headquarter.
         </p>
-        <img className="behaveImage" src="https://www.lifewire.com/thmb/XrIb-evP1ov7VMLCLnp0IILHk4E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/lans-wans-and-other-area-networks-817376-6e07f1a4121a4e13ac43660ea41ef9b9.png" alt="Local Area Network and Wide Area Network"/>
+        <img className="behaveImage" src="https://www.lifewire.com/thmb/XrIb-evP1ov7VMLCLnp0IILHk4E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/lans-wans-and-other-area-networks-817376-6e07f1a4121a4e13ac43660ea41ef9b9.png" alt="Local Area Network and Wide Area Network" width="500px"/>
     </>;
 }
