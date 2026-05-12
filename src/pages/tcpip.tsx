ec69dffe34fe731e11869763a2bf700c8568e43e
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-12 19:19:14 +1000

    Create tcpip.tsx

---
+++ src/pages/tcpip.tsx
@@ -0,0 +1,18 @@
+export function TransmissionControlProtocolSlashInternetProtocol() {
+    return <>
+        <h1>Transmission Control Protocol/Internet Protocol</h1>
+        <p>
+            TCP/IP protocol is a collection of communication protocols used to connect network devices on a network.
+        </p>
+        <p>
+            The Internet Protocol is the address system of the internet with the purpose of delivering data to a target device based on an address. Internet Protocol addresses are managed by the Internet Assigned Numbers Authority (part of ICANN) which allocate mass amounts of IPs to five Regional Internet Registrie (Asia-Pacific, Europe-Middle East-Central Asia, Canada-USA, Latin America-Carribean, and Africa), then the regional internet registries distribute these to local registries like your Internet Service Provider.
+        </p>
+        <p>
+            An Internet Protocol address is a unique identifier that is assigned to devices on a network (Can be public or private—the public ones are assigned by mass authorities, while private addresses are for use on your local network and you can't distinguish them from outside because they are the same address range with everyone's network.)
+            A heavily utilised version of the Internet Protocol is an IPv4 address—which uses 32 bit numbers, with dots between numbers.
+            Example of a private IPv4: 192.168.000.001 (shortened, common version written as 192.168.0.1, removing any non-significant digits), 
+            Loopback IPv4: 127.000.000.001 (127.0.0.1)
+            Private IPv4s start with: 10., 172.16, 172.31, 192.168. They are unable to be used on the public internet.
+        </p>
+    </>
+}
