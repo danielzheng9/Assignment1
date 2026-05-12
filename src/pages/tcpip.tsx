8b311344adbed2a24f6d87de8174662099e889f2
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-12 19:27:46 +1000

    Update tcpip.tsx

--- src/pages/tcpip.tsx
+++ src/pages/tcpip.tsx
@@ -14,5 +14,16 @@ export function TransmissionControlProtocolSlashInternetProtocol() {
             Loopback IPv4: 127.000.000.001 (127.0.0.1)
             Private IPv4s start with: 10., 172.16, 172.31, 192.168. They are unable to be used on the public internet.
         </p>
+        <p>
+            The Internet Protocol was developed within the late 1970s, and adopted in 01/01/1983 as part of TCP/IP. It was designed to connect different packet switching networks (ARPANET, radio, etc) and unify them. It replaced the Network Control Program used by APRPANET.
+            In 1981, the RFC 790 and 791 was released, providing the specification for IPv4.
+        </p>
+        <p>
+            A dynamic IP address is assigned by the router changing IP addresses by requesting new ones from the ISP. The router will request for the ISP to provide them a public IP address via DHCP, which the ISP will provide a DHCP lease to prevent stale public IP addresses not being used.
+            In homes or small offices, they would instead use a dynamic IP to rotate the IP every now and then.
+        </p>
+        <p>
+            A static IP address is assigned by the router by the router not changing IP addresses every now and then. The router will attempt to renew the license every now and then to keep it being used. This is useful for applications where you are providing a service to consumers or other businesses and need to keep the same IP—a permanent, unchanging identifier—while a dynamic IP works, it might break connections within business systems whenever an address changes, and is unreliable for large scale extended computing tasks.
+        </p>
     </>
 }
