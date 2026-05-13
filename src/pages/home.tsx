03dced7528f6463897ecac35d5ab7b54d742d9b7
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-13 18:48:56 +1000

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,7 +23,7 @@ export function Home({scrollToBottom}: {scrollToBottom?: boolean}) {
             <div className="flexNormal timelessPage">
                 <h1 ref={beginningsOfInternet}></h1>
                 <h1>The Beginnings of the Internet</h1>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Internet_map_1024_-_transparent%2C_inverted.png/330px-Internet_map_1024_-_transparent%2C_inverted.png"/>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Internet_map_1024_-_transparent%2C_inverted.png/330px-Internet_map_1024_-_transparent%2C_inverted.png" alt="The web of the internet"/>
                 <p className="ghost">an image of a portion of the internet connections [Source: <NavLink to="https://en.wikipedia.org/wiki/History_of_the_Internet">https://en.wikipedia.org/wiki/History_of_the_Internet</NavLink>]</p>
 
                 <div className="timeline">
